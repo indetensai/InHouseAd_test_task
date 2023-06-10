e7687d8d6fc10673eadab8de75c9b0440c56514d
indetensai <artyomvagin12@gmail.com>
2023-06-10 21:28:31 +0200

    style fix
    maybe final version

--- internal/app/app.go
+++ internal/app/app.go
@@ -15,41 +15,40 @@ import (
 )
 
 func Run() {
+	log.Println("startup...")
 	config := config.ReadConfig()
-	sigint := make(chan os.Signal, 1)
-	signal.Notify(sigint, os.Interrupt, syscall.SIGTERM, syscall.SIGINT, os.Kill)
-	pingService := service.NewPingService()
+
 	checkService := service.NewCheckService()
-	pingTicker := time.Tick(time.Minute)
+	pingService := service.NewPingService()
 	db := repository.ConnectToDB()
 	repo := repository.NewRepository(db)
-	result := pingService.Ping(config.Links)
-	checkService.UpdateData(result)
+
 	server := controller.NewServer(repo, config, checkService)
-	log.Println("startup...")
 	go func() {
 		if err := server.Body.ListenAndServe(); err != http.ErrServerClosed {
 			log.Fatalf("Server error: %v", err)
 		}
 	}()
+
+	pingTicker := time.Tick(time.Minute)
 	go func() {
-		for {
-			select {
-			case <-pingTicker:
-				result = pingService.Ping(config.Links)
-				checkService.UpdateData(result)
-			}
+		for ; true; <-pingTicker {
+			result := pingService.Ping(config.Links)
+			checkService.UpdateData(result)
 		}
 	}()
+
+	sigint := make(chan os.Signal, 1)
+	signal.Notify(sigint, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
 	<-sigint
 	log.Println("Shutting down server...")
+
 	counters := checkService.GetCounters()
 	repo.SaveStats(counters["specific"], counters["min"], counters["max"])
+
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	if err := server.Body.Shutdown(ctx); err != nil {
-		log.Fatalf("Server forced to shutdown: ", err)
-	}
-	log.Println("Server exiting")
+	server.Body.Shutdown(ctx)
 
+	log.Println("Server exiting")
 }
